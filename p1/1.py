a3c3744c67a08179d1850add32b62e796e658f5c
wdcjernigan <wdcjernigan@gmail.com>
2014-10-08 15:32:56 -0700

    Implements Algorithm 2
    This is a copy of A1 with an added "if" statement to not compute on
    lines marked invisible.

--- p1/1.py
+++ p1/1.py
@@ -11,9 +11,24 @@ def a1(slopes, intercepts):
                 if jkIntersectionY > i_Y:
                     visibility[i] = False
     return visibility
+	
+def a2(slopes, intercepts):
+    visibility = [True for n in xrange(len(slopes))]
+    for j in xrange(len(slopes)):
+        for i in xrange(j+1, len(slopes)):
+            for k in xrange(i+1, len(slopes)):
+				# break if line has already been marked as not visible
+				if visibility[i] != False:
+					# compute intersection
+					jkIntersectionY = slopes[j] * (intercepts[j] - intercepts[k]) + intercepts[j] * (slopes[k] - slopes[j])
+					i_Y = slopes[i] * (intercepts[j] - intercepts[k]) + intercepts[i] * (slopes[k] - slopes[j])
+					if jkIntersectionY > i_Y:
+						visibility[i] = False
+    return visibility
 
 slopes = [-2, -1, 0, 1, 2]
 intercepts = [9, 27, 54, 95, 96]
 
 print a1(slopes, intercepts)
+print a2(slopes, intercepts)
 print [True, False, False, True, True]
