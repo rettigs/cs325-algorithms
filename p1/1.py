e6e88904227dd3295cf2296cb7ba970f99e55469
wdcjernigan <wdcjernigan@gmail.com>
2014-10-08 15:24:53 -0700

    Renamed algorithm for clarity later, removed dead code.

--- p1/1.py
+++ p1/1.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-def getVisible(slopes, intercepts):
+def a1(slopes, intercepts):
     visibility = [True for n in xrange(len(slopes))]
     for j in xrange(len(slopes)):
         for i in xrange(j+1, len(slopes)):
@@ -12,15 +12,8 @@ def getVisible(slopes, intercepts):
                     visibility[i] = False
     return visibility
 
-def getY(s, i, x):
-    return s*x+i
-    
-def getIntersection(s1, i1, s2, i2):
-   return (i2 - i1) / (s1 - s2)
-   
-
 slopes = [-2, -1, 0, 1, 2]
 intercepts = [9, 27, 54, 95, 96]
 
-print getVisible(slopes, intercepts)
+print a1(slopes, intercepts)
 print [True, False, False, True, True]
